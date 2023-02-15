c325d6469869a04e6d91888ee430117950c993b8
unknown <Mustafa>
2023-02-15 09:32:17 -0800

    git push -u origin main

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+
+Plugin Name:Firsit Plugin
+Description: تمت هذه الاضالفة بواسطتي 
+Plugin URI:https://my-coded.plogspot.com
+Author: Mustafa
+Author URI: https://my-coded.plogspot.com
+
+
+*/
+?>
